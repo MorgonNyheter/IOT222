23dd7955078f22f5db4d20a5adf02b9aab247176
Anton Yemortaji <ant-life@hotmail.com>
2022-09-14 14:07:59 +0200

    heheheh

---
+++ len.py
@@ -0,0 +1,70 @@
+"""count = 5
+while count <= 11:
+    print("The number is", count)
+    count + 2
+    print("done")
+    
+count("count", count) """
+
+"""
+# Be användaren om ett nummer:
+stars = int(input("Antal: "))
+# konventera till ett heltal(int)
+
+# Börja med ett stjärna
+
+# Skriv ut ökande antal stjärnor
+star_count = 1
+while star_count <= stars:
+    print("*" * star_count)
+    star_count += 1
+
+"""
+
+# rANDOM NUMBER generation
+
+
+
+"""my_list = ["äpple", "päron", "banan"]
+random_item = random.choice(my_list)
+print(random_item) """
+
+"""
+# Be användaren skapa en sträng
+my_string = input("Skriv en sträng: ")
+# Dela upp strängen i en lista
+my_string_split = my_string.split()
+# Skriv ut varannat ord från listan
+print(my_string_split[::2])
+"""
+"""#Be användaren skapa en sträng
+my_string = input("Skriv en sträng: ")
+# Dela upp strängen i en lista
+my_string_split = my_string.split()
+# Skriv ut varannat ord från listan
+print(my_string_split[::5])
+"""
+
+"""import random
+def randomnumber():
+    return random.randint(1,10)
+print(randomnumber())
+input(randomnumber())
+"""
+
+"""
+my_string = "Här är några ord"
+str_split = my_string.split()
+
+# Börja med första ordet
+# Skriv ut vartannat ord
+
+index = 0
+
+#Loopa över listan och indixera
+while count <= len(str_split) - 1:
+    print(str_split[index])
+index += 2
+"""
+
+
